a6d0985fa2704c31bd129cb68147f5230be67abd
saka <sakanabil1221@gmail.com>
2023-10-23 13:03:12 +0700

    Pertanyaan 2

--- Pertemuan 9/ArrayRataNilai26.java
+++ Pertemuan 9/ArrayRataNilai26.java
@@ -4,18 +4,29 @@ public class ArrayRataNilai26 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[] nilaiMhs= new int[10];
-        double total = 0;
-        double rata2;
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        int jumlahMahasiswa = sc.nextInt();
+        int[] nilaiMhs = new int[jumlahMahasiswa];
+        double totalLulus = 0;
+        double totalTidakLulus = 0;
+        double rataLulus, rataTidakLulus;
+        int jmlLulus = 0;
 
         for (int i = 0; i < nilaiMhs.length; i++) {
             System.out.print("Masukkan Nilai Mahasiswa ke-"+(i+1)+" : ");
             nilaiMhs[i] = sc.nextInt();
         }
         for (int i = 0; i < nilaiMhs.length; i++) {
-            total += nilaiMhs[i];
+            if (nilaiMhs[i] > 70) {
+                totalLulus += nilaiMhs[i];
+                jmlLulus++;
+            } else {
+                totalTidakLulus += nilaiMhs[i];
+            }
         }
-        rata2 = total / nilaiMhs.length;
-        System.out.println("Rata-rata Nilai = "+rata2);
+        rataLulus = totalLulus / jmlLulus;
+        rataTidakLulus = totalTidakLulus / (nilaiMhs.length - jmlLulus);
+        System.out.println("Rata-rata Nilai Lulus = "+rataLulus);
+        System.out.println("Rata-rata Nilai Tidak Lulus = "+rataTidakLulus);
     }
 }
