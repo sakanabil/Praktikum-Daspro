f90c8324ecae9bdd81aba0e589f7192c11a42440
saka <sakanabil1221@gmail.com>
2023-10-23 12:06:55 +0700

    Pertanyaan 4

--- Pertemuan 9/ArrayNilai26.java
+++ Pertemuan 9/ArrayNilai26.java
@@ -6,12 +6,17 @@ public class ArrayNilai26 {
 
         int[] nilaiAkhir = new int[10];
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < nilaiAkhir.length; i++) {
             System.out.print("Masukkan nilai akhir ke-"+i+" : ");
             nilaiAkhir[i] = sc.nextInt();
         }
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Nilai akhir ke-"+i+" adalah "+nilaiAkhir[i]);
+
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            if (nilaiAkhir[i] > 70) {
+                System.out.println("Mahasiswa ke-"+i+" Lulus!");
+            } else {
+                System.out.println("Mahasiswa ke-"+i+" Tidak Lulus!");
+            }
         }
     }
 }
