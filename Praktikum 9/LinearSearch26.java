f3192b54af09460781d9385e5e5f90346ef82032
saka <sakanabil1221@gmail.com>
2023-10-25 11:27:30 +0700

    Modifikasi 3

--- Praktikum 9/LinearSearch26.java
+++ Praktikum 9/LinearSearch26.java
@@ -1,17 +1,38 @@
+import java.util.Scanner;
+
 public class LinearSearch26 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        int[] arrayInt = {34, 18, 26, 48, 72, 20, 56, 63};
-        int key = 20;
+        System.out.print("Masukkan jumlah elemen array: "); // input jumlah elemen
+        int jmlhArray = sc.nextInt();
+        int[] arrayInt = new int[jmlhArray]; // inisialisasi array
+        int key;
         int hasil = 0;
 
+        // looping for input elemen array
+        for (int i = 0; i < arrayInt.length; i++) {
+            System.out.print("Masukkan elemen array ke-" + i + ": ");
+            arrayInt[i] = sc.nextInt();
+        }
+        
+        // input key yang ingin dicari
+        System.out.print("Masukkan key yang ingin dicari: ");
+        key = sc.nextInt();
+
+        // looping untuk mencari key
         for (int i = 0; i < arrayInt.length; i++) {
             if (arrayInt[i] == key) {
                 hasil = i;
                 break;
             }
         }
-
-        System.out.println("Key ada dalam array pada posisi indeks ke-"+hasil);
+        
+        // output
+        if (hasil != 0) {
+            System.out.println("Key ada di posisi indeks ke-" + hasil);
+        } else {
+        System.out.println("Key tidak ditemukan");
+        }
     }
 }
