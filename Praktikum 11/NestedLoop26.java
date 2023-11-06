c629200f805000732817b6bc42a171fd32a69ff0
saka <sakanabil1221@gmail.com>
2023-11-06 14:58:12 +0700

    Pertanyaan 3

--- Praktikum 11/NestedLoop26.java
+++ Praktikum 11/NestedLoop26.java
@@ -3,22 +3,26 @@ import java.util.Scanner;
 public class NestedLoop26 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double [][] temps = new double[5][7];
+        double[][] temps = new double[5][7];
+        double[] rataRata = new double[temps.length];
 
-        for (int i=0; i<temps.length; i++) {
+        for (int i = 0; i < temps.length; i++) {
             System.out.println("Kota ke-" + i);
-            for (int j=0; j<temps[0].length; j++) {
-                System.out.print("Hari ke-" + (j+1) + ": ");
+            for (int j = 0; j < temps[0].length; j++) {
+                System.out.print("Hari ke-" + (j + 1) + ": ");
                 temps[i][j] = sc.nextDouble();
+                rataRata[i] += temps[i][j];
             }
+            rataRata[i] /= temps[0].length;
             System.out.println();
         }
-        for (int i=0; i<temps.length; i++) {
-            System.out.print("Kota ke-" + (i+1) + ": ");
-            for (int j=0; j<temps[0].length; j++) {
-                System.out.print(temps[i][j] + " ");
+        for (int i = 0; i < temps.length; i++) {
+            System.out.print("Kota ke-" + (i + 1) + ": ");
+            for (double temp : temps[i]) {
+                System.out.print(temp + " ");
             }
             System.out.println();
+            System.out.println("Rata-rata: " + rataRata[i]);
         }
     }
 }
