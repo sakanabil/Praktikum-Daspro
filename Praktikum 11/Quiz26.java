f730f5233acd52bd5623a171ee3e8943fd883415
saka <sakanabil1221@gmail.com>
2023-11-06 14:20:09 +0700

    Pertanyaan 3

--- Praktikum 11/Quiz26.java
+++ Praktikum 11/Quiz26.java
@@ -3,22 +3,29 @@ import java.util.Random;
 
 public class Quiz26 {
     public static void main(String[] args) {
-    
-    Random random = new Random();
-    Scanner input = new Scanner(System.in);
+        Random random = new Random();
+        Scanner input = new Scanner(System.in);
 
-    char menu='y';
-    do {
-        int number = random.nextInt(10) + 1;
-        boolean success = false;
+        char menu = 'y';
         do {
-            System.out.print("Tebak angka (1-10) : ");
-            int answer = input.nextInt();
-            input.nextLine();
-            success = (answer == number);
-        } while(!success);
-        System.out.print("Apakah Anda ingin mengulang permainan (Y/y)? ");
-        menu = input.nextLine().charAt(0);
-    }while(menu == 'y' || menu == 'Y');
-}
+            int number = random.nextInt(10) + 1;
+            boolean success = false;
+            do {
+                System.out.print("Tebak angka (1-10): ");
+                int answer = input.nextInt();
+                input.nextLine();
+
+                if (answer < number) {
+                    System.out.println("Lebih kecil dari jawaban benar");
+                } else if (answer > number) {
+                    System.out.println("Lebih besar dari jawaban benar");
+                } else {
+                    success = true;
+                }
+                success = (answer == number);
+            } while (!success);
+            System.out.println("Apakah Anda ingin mengulang permaian (Y/y)?");
+            menu = input.nextLine().charAt(0);
+        } while (menu == 'Y' || menu == 'y');
+    }
 }
