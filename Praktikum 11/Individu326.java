eae52f21433ce6072f923246a3eb08a8d763143d
saka <sakanabil1221@gmail.com>
2023-11-13 14:31:07 +0700

    Perubahan

--- Praktikum 11/Individu326.java
+++ Praktikum 11/Individu326.java
@@ -1,20 +1,20 @@
 import java.util.Scanner;
 
-public class Triangle26 {
+public class Individu326 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Masukkan nilai N = ");
         int N = sc.nextInt();
-        int i=0;
+        int i=1;
         while(i<=N){
             int j=0;
-            while(j<i){
-                System.out.print("*");
+            while(j<N){
+                System.out.print("  *  ");
                 j++;
             }
             i++;
-            System.out.println();
+            System.out.println("  *  ");
         }
     }
 }
