76794bb3ab6630b43289c9f7422b5b678a6df301
saka <sakanabil1221@gmail.com>
2023-11-20 14:03:47 +0700

    Jobsheet 13

--- Praktikum 13/TerimaKasih_26.java
+++ Praktikum 13/TerimaKasih_26.java
@@ -4,7 +4,12 @@ public class TerimaKasih_26 {
     public static void UcapanTerimakasih() {
         System.out.println("Thank you for being the best teacher in the world.\n"+"You inspired in me a love for learning and made me feel like I could ask you anything.");
     }
+    public static void UcapanTambahan(String ucapan){
+        System.out.println(ucapan);
+    }
     public static void main(String[] args) {
         UcapanTerimakasih();
+        String ucapan = "Terima Kasih Pak.. Bu.. Semoga Sehat Selalu";
+        UcapanTambahan(ucapan);
     }
 }
